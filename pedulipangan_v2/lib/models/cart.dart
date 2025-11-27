a1d646ac71f7d794462aa5bd82d17f61b38edfe5
sadrakhxvid777@gmail.com <sadrakhxvid777@gmail.com>
2025-11-27 18:02:35 +0700

    add new cart models

---
+++ pedulipangan_v2/lib/models/cart.dart
@@ -0,0 +1,42 @@
+import 'menu.dart';
+
+class CartItems {
+  final OrderableMeal meal;
+  final DateTime date;
+  int quantity;
+
+  CartItems({
+    required this.meal,
+    required this.date,
+    this.quantity = 1,
+  });
+}
+
+class ShoppingCart {
+  final List<CartItems> items = [];
+
+  void addItem(OrderableMeal meal, DateTime date) {
+    bool found = false;
+    for (var item in items) {
+      if (item.meal == meal && item.date.isAtSameMomentAs(date)) {
+        item.quantity++;
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      items.add(CartItems(meal: meal, date: date));
+    }
+  }
+
+  void decrementItem(CartItems item) {
+    if (item.quantity > 1) {
+      item.quantity--;
+    } else {
+      items.remove(item);
+    }
+  }
+  int get totalItems => items.fold(0, (sum, item) => sum + item.quantity);
+}
+
+final ShoppingCart globalCart = ShoppingCart();
