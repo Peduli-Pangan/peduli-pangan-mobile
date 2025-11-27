a6036428763085c3c26c2c21c7fa84b6e5163b1c
sadrakhxvid777@gmail.com <sadrakhxvid777@gmail.com>
2025-11-27 13:18:41 +0700

    add: apps theme

---
+++ pedulipangan_v2/lib/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryGreen = Color(0xFF1C884A);
+  static const Color background = Color(0xFFF5F5F5);
+  static const Color textPrimary = Color(0xFF2D2D2D);
+  static const Color textGrey = Color(0xFF757575);
+}
