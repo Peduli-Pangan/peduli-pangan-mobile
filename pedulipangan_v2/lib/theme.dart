6bd948e740d1b099faecbf80b9b8ff7ff120d9c8
sadrakhxvid777@gmail.com <sadrakhxvid777@gmail.com>
2025-11-29 19:05:41 +0700

    add: secondarygreen const

--- pedulipangan_v2/lib/theme.dart
+++ pedulipangan_v2/lib/theme.dart
@@ -5,4 +5,5 @@ class AppColors {
   static const Color background = Color(0xFFF5F5F5);
   static const Color textPrimary = Color(0xFF2D2D2D);
   static const Color textGrey = Color(0xFF757575);
+  static const Color secondaryGreen = Color.fromARGB(255, 110, 189, 144);
 }
